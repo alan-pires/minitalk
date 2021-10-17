bdf53c5f2a60075f7a82346762e051b805719671
Alan <alan_pires2@hotmail.com>
2021-10-16 21:34:05 -0300

    100%

--- client_bonus.c
+++ client_bonus.c
@@ -6,21 +6,18 @@
 /*   By: apires-d <apires-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 17:12:03 by apires-d          #+#    #+#             */
-/*   Updated: 2021/10/16 19:15:40 by apires-d         ###   ########.fr       */
+/*   Updated: 2021/10/16 21:33:46 by apires-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void		send_msg(char *msg, pid_t pid_server);
-static int		ft_atoi(const char *nptr);
-static void		handle_signal(int sig);
-
-int	g_server_confirm;
+static void	send_msg(char *msg, pid_t pid_server);
+static int	ft_atoi(const char *nptr);
+static void	handle_signal(int sig);
 
 int	main(int argc, char **argv)
 {
-	g_server_confirm = 1;
 	if (argc != 3)
 	{
 		write(2, "Usage: ./client PID MESSAGE\n", 28);
@@ -32,13 +29,7 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	signal(SIGUSR1, handle_signal);
-	if (g_server_confirm == 1)
-	{
-		send_msg(argv[2], ft_atoi(argv[1]));
-		g_server_confirm = 0;
-	}
-	while (42)
-		pause();
+	send_msg(argv[2], ft_atoi(argv[1]));
 	return (0);
 }
 
@@ -51,15 +42,13 @@ static void	send_msg(char *msg, pid_t pid_server)
 		aux = 0;
 		while (aux < 8)
 		{
-			if (signal(SIGUSR1, handle_signal))
-			{
-				if (*msg & (128 >> aux))
-					kill(pid_server, SIGUSR1);
-				else
-					kill(pid_server, SIGUSR2);
-				aux++;
-				usleep(10000);
-			}
+			signal(SIGUSR1, handle_signal);
+			if (*msg & (128 >> aux))
+				kill(pid_server, SIGUSR1);
+			else
+				kill(pid_server, SIGUSR2);
+			aux++;
+			usleep(10000);
 		}
 		msg++;
 	}
@@ -68,7 +57,7 @@ static void	send_msg(char *msg, pid_t pid_server)
 static void	handle_signal(int sig)
 {
 	if (sig == SIGUSR1)
-		g_server_confirm = 1;
+		write(1, ":) ", 3);
 }
 
 static int	ft_atoi(const char *nptr)
